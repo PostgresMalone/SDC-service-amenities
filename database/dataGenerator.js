3c64f52425a4bb44bd79f7f80c8abe81f12a16bf
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-11 11:34:34 -0800

    Delete commented out code related to the previous commit

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -1,9 +1,7 @@
 const faker = require('faker');
 const fs = require('fs');
 
-const amenities = ['airConditioning', 'bathroomEssentials', 'bedroomComforts', 'carbonMonoxideDetector', 'coffeeMaker', 'dishWasher', 'dryer', 'hairDryer', 'heating', 'indoorFireplace', 'iron', 'kitchen', 'petsAllowed', 'pool', 'roomname', 'selfCheckIn', 'smokeDetector', 'TV', 'washer', 'wifi']; 
-// const specialAmenities = ['selfCheckIn', 'pool','kitchen','washer','dryer','dishwasher','indoorFireplace','petsAllowed','heating','airConditioning'];
-// const essentialAmenities = ['wifi', 'TV', 'bathroomEssentials', 'bedroomComforts', 'coffeeMaker', 'hairDryer', 'iron', 'carbonMonoxideDetector', 'smokeDetector'];
+const amenities = ['airConditioning', 'bathroomEssentials', 'bedroomComforts', 'carbonMonoxideDetector', 'coffeeMaker', 'dishWasher', 'dryer', 'hairDryer', 'heating', 'indoorFireplace', 'iron', 'kitchen', 'petsAllowed', 'pool', 'roomname', 'selfCheckIn', 'smokeDetector', 'TV', 'washer', 'wifi'];
 
 const columns = 'id,' + amenities.join() + '\n';
 const before = Date.now();
@@ -19,14 +17,6 @@ const writeFile = (stream, rowsRecorded, j) => {
 
   const id = (j - 1) * 1000000 + rowsRecorded + 1;
   let roomString = `${id}`;
-  
-  // for (let j = 0; j < specialAmenities.length; j++) {
-  //   roomString = roomString + ',' + !!Math.floor(Math.random() * 2);
-  // }
-
-  // for (let k = 0; k < essentialAmenities.length; k++) {
-  //   roomString = roomString + ',' + !!Math.floor(Math.random() * 2);
-  // }
 
   for (let k = 0; k < amenities.length; k++) {
     if (amenities[k] === 'roomname') {
--- database/loopDataSaver.sh
+++ database/loopDataSaver.sh
@@ -4,7 +4,6 @@ counter=1
 while [ $counter -le 10 ]
   do
   sed -i '' s/[0-9][0-9]*/$counter/g ./database/dataSaver.cql
-  # cat database/dataSaver.cql
   cqlsh -f database/dataSaver.cql
   ((counter++))
 done

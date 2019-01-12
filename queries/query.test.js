be36faf4e92a74ff5facbef65442a136c22fa7eb
Felipe Parreira <felipeparreirafb@gmail.com>
2019-01-11 22:38:56 -0800

    Add tests for Postgres queries

--- queries/query.test.js
+++ queries/query.test.js
@@ -1,77 +1,133 @@
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   user: 'lucasparreiradefariaborges',
-//   host: 'localhost',
-//   database: 'ROOMSAMENITIES',
-//   password: ''
-// });
-
-// pool.on('error', (err) => {
-//   console.log('Error', err);
-// });
-
-// // describe('sample test', () => {
-// //   test('sample select query', async () => {
-// //     await pool.query('select * from rooms where id = 1', async (err, res) => {
-// //       console.log('i am here!2')
-// //       if (err) {
-// //         console.log('error!!!');
-// //         pool.end();
-// //       } else {
-// //         console.log(res.rows);
-// //         console.log('hello2');
-// //         expect(1).toBe(1);
-// //         pool.end();
-// //       }
-// //     });
-// //   });
-// // });
-// // console.time('id=3969240');
-// // // const before = Date.now();
-// // pool.query('select * from rooms where id = 3969240', (err, res) => {
-// //   if (err) {
-// //     throw err;
-// //   } else {
-// //     console.log(res.rows);
-// //     console.timeEnd('id=3969240');
-// //     // console.log('time taken:', Date.now() - before);
-// //   }
-// // });
-
-// // console.time('id=occaecati3969239');
-// // pool.query('select * from rooms where roomname = \'occaecati3969239\'', (err, res) => {
-// //   if (err) {
-// //     throw err;
-// //   } else {
-// //     console.log(res.rows);   
-// //     console.timeEnd('id=occaecati3969239');
-// //   }
-// // });
-
-// pool.end();
-
+const { Pool } = require('pg');
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({ contactPoints: ['localhost'], localDataCenter: 'datacenter1', keyspace: 'roomsamenities' });
-	
-client.connect(function (err) {
-  if (err) {
-    return console.error(err); 
-  }
-  console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
-  console.time('id=3969240');
-  client.execute('SELECT * FROM "Rooms" WHERE id = 9900000', [], { prepare: true })
-    .then((result) => {
-      console.log('data saved');
-      console.log(result.rows);
-      console.timeEnd('id=3969240');
-      client.shutdown();
-    })
-    .catch(err => {
-      console.log('error', err);
-      client.shutdown();
+const _ = require('underscore');
+
+describe('SELECT by id query for Postgres', () => {
+  let ids;
+  let pool;
+
+  beforeEach(async () => {
+    pool = new Pool({
+      user: 'lucasparreiradefariaborges',
+      host: 'localhost',
+      database: 'ROOMSAMENITIES',
+      password: ''
+    });
+    
+    pool.on('error', (err) => {
+      console.log('Error', err);
     });
 
+    const { rows } = await pool.query('SELECT * FROM rooms LIMIT 10 OFFSET 9000000');
+    ids = _.pluck(rows, 'id');
+  });
+
+  afterEach(() => {
+    pool.end();
+  });
+
+  test('it should SELECT the 10 queries from the end of the table', () => {
+    expect(ids.length).toBe(10);
+  });
+
+  test('it should execute the queries in less than 50ms each in average', async () => {
+    const promisedQueryResults = [];
+    const before = Date.now();
+
+    for (let i = 0; i < ids.length; i++) {
+      promisedQueryResults.push(pool.query(`SELECT * FROM rooms WHERE id = ${ids[i]}`));
+    }
+
+    await Promise.all(promisedQueryResults)
+      .then(() => {
+        const after = Date.now();
+        expect(after - before).toBeLessThan(500);
+      });
+  });
+});
+
+describe('SELECT by roomname query for Postgres', () => {
+  let roomnames;
+  let pool;
+
+  beforeEach(async () => {
+    pool = new Pool({
+      user: 'lucasparreiradefariaborges',
+      host: 'localhost',
+      database: 'ROOMSAMENITIES',
+      password: ''
+    });
+    
+    pool.on('error', (err) => {
+      console.log('Error', err);
+    });
+
+    const { rows } = await pool.query('SELECT * FROM rooms LIMIT 10 OFFSET 9000000');
+    roomnames = _.pluck(rows, 'roomname');
+  });
+
+  afterEach(() => {
+    pool.end();
+  });
+
+  test('it should SELECT the 10 queries from the end of the table', () => {
+    expect(roomnames.length).toBe(10);
+  });
+
+  test('it should execute the queries in less than 50ms each in average', async () => {
+    const promisedQueryResults = [];
+    const before = Date.now();
+
+    for (let i = 0; i < roomnames.length; i++) {
+      promisedQueryResults.push(pool.query(`SELECT * FROM rooms WHERE roomname = '${roomnames[i]}'`));
+    }
+
+    await Promise.all(promisedQueryResults)
+      .then(() => {
+        const after = Date.now();
+        expect(after - before).toBeLessThan(500);
+      });
+  });
+});
+
+//====================================================================================
+
+describe('SELECT by id query for Cassandra', () => {
+  let ids;
+  let client;
+
+  beforeEach(async () => {
+    client = new cassandra.Client({ contactPoints: ['localhost'], localDataCenter: 'datacenter1', keyspace: 'roomsamenities' });
+
+    await client.connect();
+    const { rows } = await client.execute('SELECT * FROM "Rooms" WHERE id = 9900000', [], { prepare: true });
+    ids = _.pluck(rows, 'id');
+
+  });
+
+  afterEach(() => {
+    client.shutdown();
+  });
+
+  test('it should SELECT the 10 queries from the end of the table', () => {
+    expect(ids.length).toBe(1);
+    console.log('length', ids);
+  });
+
+  // test('it should execute the queries in less than 50ms each in average', async () => {
+  //   const promisedQueryResults = [];
+  //   const before = Date.now();
+
+  //   for (let i = 0; i < ids.length; i++) {
+  //     promisedQueryResults.push(pool.query(`SELECT * FROM rooms WHERE id = ${ids[i]}`));
+  //   }
+
+  //   await Promise.all(promisedQueryResults)
+  //     .then(() => {
+  //       const after = Date.now();
+  //       expect(after - before).toBeLessThan(500);
+  //     });
+  // });
 });
 
 // client.connect(function (err) {
